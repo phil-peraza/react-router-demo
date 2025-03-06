312554fadef71170185d9222a4c04bcbd55ab455
Felipe Peraza <phil.peraza@outlook.com>
2025-03-06 04:24:01 -0800

    feat/ add navigation bar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,20 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "../Dashboard/Dashboard";
+import Reviews from "../Reviews/Reviews";
+import AboutMe from "../AboutMe/AboutMe";
 import Header from "../Header/Header";
+import "./App.css";
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/reviews" element={<Reviews />} />
+          <Route path="/about-me" element={<AboutMe />} />
+        </Routes>
     </div>
   );
 }
